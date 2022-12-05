980c8f8d4244da76eb39b42d4cdba1b84baf15fa
niki <Nikitatsivinsky@gmail.com>
2022-12-05 23:37:17 +0200

    lesson 1

--- lesson_1/lesson_1.py
+++ lesson_1/lesson_1.py
@@ -1,18 +1,18 @@
 from pprint import pprint
-def task_separator(task_number):
-    task_number = ' ' + task_number + ' '
-    txt = '|{txt:*^70}|'.format(txt=task_number)
+def task_separator(task_name):
+    task_name = ' ' + task_name + ' '
+    txt = '|{txt:*^70}|'.format(txt=task_name)
     print(f'\n {txt}')
 
 # 1 Task ver.2
-task_separator(task_number='1 Task ver.2')
+task_separator(task_name='1 Task ver.2')
 
 text = 'I love Python'
 for i in range(42):
     pprint(text)
 
 # 1 Task ver.1
-task_separator(task_number='1 Task ver.1')
+task_separator(task_name='1 Task ver.1')
 
 pprint(f'{text*42}')
 
@@ -22,14 +22,14 @@ age_in_month = my_age_int * 12
 age_in_years = age_in_month // 12
 
 # 3 Task
-task_separator(task_number='3 Task')
+task_separator(task_name='3 Task')
 
 my_name = 'Nikita'
 my_age = f'“Му name is {my_name}, I’m {my_age_int} years old”'
 print(my_age)
 
 # 4 Task
-task_separator(task_number='4 Task')
+task_separator(task_name='4 Task')
 
 task_5_value = 1
 print(task_5_value > my_age_int)
@@ -44,7 +44,7 @@ print(
       )
 
 # 5 Task
-task_separator(task_number='5 Task')
+task_separator(task_name='5 Task')
 
 a=2
 b=5
