514a6f4a3c9d5755c9bd4396c7917258ade60394
niki <Nikitatsivinsky@gmail.com>
2022-12-14 00:15:37 +0200

    add version 3 (Match\Case with RegEx and Exception)

--- lesson_5/lesson_5.py
+++ lesson_5/lesson_5.py
@@ -1,6 +1,47 @@
 # coded: UTF-8
 
-# 1 Task Version 2
+# 1 Task Version 3 (Match\Case with RegEx and Exception)
+
+import re
+
+input_value = input('Зробіть введення значення: ')
+
+string_regex = "^[a-zA-Zа-яА-ЯёЁЄєІіЇї]+$"
+symbol_regex = "^[&^_@#\-!$%^&*()£€¥§°¢_+|~=`{}\[\]:\";'<>?,.\/\\\]+$"
+number_regex = "^\d+$"
+
+string_pattern = re.compile(string_regex)
+symbol_pattern = re.compile(symbol_regex)
+number_pattern = re.compile(number_regex)
+
+for symbol in input_value:
+    match bool(number_pattern.search(symbol)):
+        case True:
+            print('Це “число”')
+            match int(symbol) % 2 == 0:
+                case True:
+                    print(f'Число “{symbol}“ парне.')
+                case False:
+                    print(f'Число “{symbol}“ НЕ парне.')
+        case False:
+            match bool(string_pattern.search(symbol)):
+                case True:
+                    print('Це “буква”')
+                    match symbol.upper() == symbol:
+                        case True:
+                            print(f'Буква “{symbol}“ велика.')
+                        case False:
+                            print(f'Буква “{symbol}“ маленька.')
+                case False:
+                    match bool(symbol_pattern.search(symbol)):
+                        case True:
+                            print(f'Це “символ" - {symbol}')
+                        case False:
+                            raise TypeError('Як ти це зробив?')
+
+    print("___________________")
+
+# 1 Task Version 2 (if-else with RegEx)
 
 import re
 
@@ -30,14 +71,14 @@ for symbol in input_value:
             print(f'Буква “{symbol}“ маленька.')
 
     elif symbol_pattern.search(symbol):
-        print(f'Це “символ - {symbol}”')
+        print(f'Це “символ" - {symbol}')
 
     else:
         print(f"Як ти це зробив?")
 
     print("___________________")
 
-# 1 Task Version 1
+# 1 Task Version 1 (if-else with Functions)
 
 input_value = input('Зробіть введення значення: ')
 
@@ -68,5 +109,3 @@ import time
 while True:
     print("“I love Python”")
     time.sleep(4.2)
-
-
