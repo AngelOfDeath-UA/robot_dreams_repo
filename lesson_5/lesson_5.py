f3262711fd5a637fec1c35a6655facee3aac8764
niki <Nikitatsivinsky@gmail.com>
2022-12-13 23:47:56 +0200

    lesson_5 added version with functions for input value and added into version with regular expressions - cycle.

--- lesson_5/lesson_5.py
+++ lesson_5/lesson_5.py
@@ -1,6 +1,6 @@
 # coded: UTF-8
 
-# 1 Task
+# 1 Task Version 2
 
 import re
 
@@ -14,25 +14,52 @@ string_pattern = re.compile(string_regex)
 symbol_pattern = re.compile(symbol_regex)
 number_pattern = re.compile(number_regex)
 
-if len(input_value) == 1:
-    if number_pattern.search(input_value):
+for symbol in input_value:
+    if number_pattern.search(symbol):
         print('Це “число”')
-        if int(input_value) % 2 == 0:
-            print(f'Число “{input_value}“ парне.')
+        if int(symbol) % 2 == 0:
+            print(f'Число “{symbol}“ парне.')
         else:
-            print(f'Число “{input_value}“ НЕ парне.')
+            print(f'Число “{symbol}“ НЕ парне.')
 
-    elif string_pattern.search(input_value):
+    elif string_pattern.search(symbol):
         print('Це “буква”')
-        if input_value.upper() == input_value:
-            print(f'Буква “{input_value}“ велика.')
+        if symbol.upper() == symbol:
+            print(f'Буква “{symbol}“ велика.')
         else:
-            print(f'Буква “{input_value}“ маленька.')
+            print(f'Буква “{symbol}“ маленька.')
 
-    elif symbol_pattern.search(input_value):
-        print('Це “символ”')
-else:
-    print('Введіть один символ або літеру або символ.')
+    elif symbol_pattern.search(symbol):
+        print(f'Це “символ - {symbol}”')
+
+    else:
+        print(f"Як ти це зробив?")
+
+    print("___________________")
+
+# 1 Task Version 1
+
+input_value = input('Зробіть введення значення: ')
+
+for symbol in input_value:
+    if symbol.isdigit():
+        print('Це “число”')
+        if int(symbol) % 2 == 0:
+            print(f'Число “{symbol}“ парне.')
+        else:
+            print(f'Число “{symbol}“ НЕ парне.')
+    elif not symbol.isalnum():
+        print(f'Це “символ - {symbol}”')
+    elif symbol.isalpha():
+        print('Це “буква”')
+        if symbol.upper() == symbol:
+            print(f'Буква “{symbol}“ велика.')
+        else:
+            print(f'Буква “{symbol}“ маленька.')
+    else:
+        print(f"Як ти це зробив?")
+
+    print("___________________")
 
 # 2 Task
 
