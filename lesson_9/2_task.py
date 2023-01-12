1fed0fb4e23164f1efbe226b8ca533607c951daf
niki <Nikitatsivinsky@gmail.com>
2023-01-12 16:03:48 +0200

    Lesson_9

---
+++ lesson_9/2_task.py
@@ -0,0 +1,64 @@
+# 2. Написати декоратор, який буде записувати в файл назв
+# у функції, яку він декорує, і писати час її виклику.
+
+# 2 Task with CSV
+
+# import time
+# import csv
+# from datetime import datetime
+#
+#
+# def time_decorator(func):
+#     def time_track(num):
+#         with open('timetrack.csv', 'a+', newline='') as out_csv:
+#             writer = csv.writer(out_csv)
+#             writer.writerow([str(func), str(datetime.now())])
+#             func(num)
+#
+#     return time_track
+#
+# @time_decorator
+# def myfunc1(i):
+#     x = i ** 10000
+#
+#
+# @time_decorator
+# def myfunc2(x):
+#     x += 10
+#     time.sleep(3)
+#
+#
+# if __name__ == "__main__":
+#     myfunc1(100)
+#     myfunc2(30)
+
+# 2 Task with Logging
+
+# import time
+# import logging
+# from datetime import datetime
+#
+#
+# def time_decorator(func):
+#     def time_track(num):
+#         logging.debug(f'Name func: {func} | Time: {datetime.now()}')
+#         func(num)
+#
+#     return time_track
+#
+#
+# @time_decorator
+# def myfunc1(i):
+#     x = i ** 10000
+#
+#
+# @time_decorator
+# def myfunc2(x):
+#     x += 10
+#     time.sleep(3)
+#
+#
+# if __name__ == "__main__":
+#     logging.basicConfig(level=logging.DEBUG, filename='timetrack.log')
+#     myfunc1(100)
+#     myfunc2(30)
