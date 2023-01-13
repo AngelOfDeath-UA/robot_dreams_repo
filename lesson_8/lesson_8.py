eb4b8e0f0846eac38207e52bb6f05e7a1ea0d2bb
niki <Nikitatsivinsky@gmail.com>
2023-01-13 17:36:18 +0200

    Lesson_8 2 Commit

--- lesson_8/lesson_8.py
+++ lesson_8/lesson_8.py
@@ -2,104 +2,117 @@
 
 # Task 1 Time Decorator
 
-# import time
-# from datetime import datetime
-#
-#
-# def time_decorator(func):
-#     def time_track(num):
-#         print(func)
-#         print(datetime.now())
-#         func(num)
-#
-#     return time_track
-#
-#
-# @time_decorator
-# def myfunc1(i):
-#     x = i ** 10000
-#
-#
-# @time_decorator
-# def myfunc2(x):
-#     x += 10
-#     time.sleep(3)
-#
-#
-# if __name__ == "__main__":
-#     myfunc1(100)
-#     myfunc2(30)
+import time
+from datetime import datetime
+
+
+def time_decorator(func):
+    def time_track(*args, **kwargs):
+        print(func)
+        print(datetime.now())
+        return func(*args, **kwargs)
+
+    return time_track
+
+
+@time_decorator
+def myfunc1(i):
+    i **= 10000
+
+
+@time_decorator
+def myfunc2(x):
+    x += 10
+    time.sleep(3)
+
+
+if __name__ == "__main__":
+    myfunc1(100)
+    myfunc2(30)
 
 # Task 1 Time Decorator with Logging
 
-# import time
-# import logging
-# from datetime import datetime
-#
-#
-# def time_decorator(func):
-#     def time_track(num):
-#         logging.debug(f'Name func: {func} | Time: {datetime.now()}')
-#         func(num)
-#
-#     return time_track
-#
-#
-# @time_decorator
-# def myfunc1(i):
-#     x = i ** 10000
-#
-#
-# @time_decorator
-# def myfunc2(x):
-#     x += 10
-#     time.sleep(3)
-#
-#
-# if __name__ == "__main__":
-#     logging.basicConfig(level=logging.DEBUG, filename='time_decorator.log')
-#     myfunc1(100)
-#     myfunc2(30)
+import time
+import logging
+from datetime import datetime
+
+
+def time_decorator(func):
+    def time_track(num):
+        logging.debug(f'Name func: {func} | Time: {datetime.now()}')
+        func(num)
+
+    return time_track
+
+
+@time_decorator
+def myfunc1(i):
+    i **= 10000
+
+
+@time_decorator
+def myfunc2(x):
+    x += 10
+    time.sleep(3)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG, filename='time_decorator.log')
+    myfunc1(100)
+    myfunc2(30)
 
 # Task 2 Custom Exception Class
 
-# class MyCustomException(Exception):
-#     def __init__(self, message="Custom exception is occured"):
-#         self.message = message
-#         super().__init__(self.message)
-#
-#
-# raise MyCustomException()
+class MyCustomException(Exception):
+    def __init__(self, message="Custom exception is occured"):
+        self.message = message
+        super().__init__(self.message)
+
+
+raise MyCustomException()
 
 # Task 3 Custom Context Manager
 
-# file_name = 'test.txt'
-#
-# with open(file_name) as file:
-#     for line in file:
-#         print('=' * 10)
-#         print(line)
+class CustomContextManager:
+
+    @staticmethod
+    def print_file(filename, mode):
+        with open(filename, mode) as file:
+            for string in file:
+                print(string)
+
+    def __enter__(self):
+        print("=" * 10)
+
+    def __exit__(self, exc_type, exc_value, exc_tb):
+        print("=" * 10)
+
+
+filename = "test.txt"
+mode = 'r'
+with CustomContextManager() as file:
+    CustomContextManager.print_file(filename, mode)
 
 # Task 4 Custom Context Manager with try|except|else
 
-# def custom_context_manager(file_name):
-#     try:
-#         file = open(file_name)
-#     except FileNotFoundError as exp:
-#         print(f'File {exp.filename} exists!')
-#     except Exception as exp:
-#         print(f'An error occurred: {exp}')
-#     else:
-#         try:
-#             for line in file:
-#                 print('=' * 10)
-#                 print(line)
-#         except UnicodeDecodeError as exp:
-#             print(f'Inside file {file_name} error with UTF-8!')
-#         except Exception as exp:
-#             print(exp)
-#         file.close()
-#
-#
-# file_name = 'test.txt'
-# custom_context_manager(file_name)
+def custom_context_manager(file_name):
+    try:
+        file = open(file_name)
+    except FileNotFoundError as exp:
+        print(f'File {exp.filename} exists!')
+    except Exception as exp:
+        print(f'An error occurred: {exp}')
+    else:
+        try:
+            for line in file:
+                print('=' * 10)
+                print(line)
+        except UnicodeDecodeError as exp:
+            print(f'Inside file {file_name} error with UTF-8!')
+        except Exception as exp:
+            print(exp)
+        file.close()
+
+
+file_name = 'test.txt'
+custom_context_manager(file_name)
