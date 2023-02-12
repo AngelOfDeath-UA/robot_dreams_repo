f3859a9d6bef72722908a8d7d2cbbed19b1016f2
niki <Nikitatsivinsky@gmail.com>
2023-02-12 22:10:42 +0200

    lesson 12 SQLLite DDL & DML commands added age

--- lesson_12/lesson_12.sql
+++ lesson_12/lesson_12.sql
@@ -3,17 +3,18 @@ CREATE TABLE
       id INTEGER PRIMARY KEY AUTOINCREMENT,
       first_name TEXT NOT NULL,
       last_name TEXT NOT NULL,
+      age INTEGER,
       UNIQUE (first_name, last_name)
     )
 ;
 
 INSERT INTO
-    users (first_name, last_name)
+    users (first_name, last_name, age)
         VALUES
-          ('John', 'Doe'),
-          ('Jane', 'Doe'),
-          ('Jim', 'Smith'),
-          ('Emily', 'Johnson'),
-          ('Michael', 'Brown')
+          ('John', 'Doe', 23),
+          ('Jane', 'Doe', 18),
+          ('Jim', 'Smith', 27),
+          ('Emily', 'Johnson', 19),
+          ('Michael', 'Brown', 33)
     ;
 
