463f21d61af44a31ce3dcbc7b895acf793575a1e
niki <Nikitatsivinsky@gmail.com>
2023-04-10 22:34:34 +0300

    HW 22 - debug

--- django/robot_dreams/robot_dreams/settings.py
+++ django/robot_dreams/robot_dreams/settings.py
@@ -30,10 +30,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 
 x = os.getenv('SECRET_KEY')
-SECRET_KEY = x = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = x = os.getenv('DEBUG')
+DEBUG = os.getenv('DEBUG')
 
 ALLOWED_HOSTS = []
 
